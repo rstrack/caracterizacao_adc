12ec0346176294125366bf809e3516fc45c56dfe
rstrack <strackinado@gmail.com>
2022-12-21 23:11:05 -0300

    correção na escrita de linhas no arquivo csv

--- serial to excel/serialtosheet.py
+++ serial to excel/serialtosheet.py
@@ -7,8 +7,7 @@ TEMPOMAXIMO = 3 #SEGUNDOS
 with open('./dados.csv', "w") as file:
     s = serial.Serial("COM3", 1000000)
     t_inicio = time()
-    file.write('Tempo(us);Tempo(ms);Valor')
-    file.write(' ; ; ')
+    file.write('Tempo(us);Tempo(ms);Valor\r')
     while time() - t_inicio < TEMPOMAXIMO:
         linha = s.readline().decode().replace('\n', '')
         file.write(linha)
